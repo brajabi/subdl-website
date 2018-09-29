e77abb04e257eeb9996a40472f611f4d0b543ed0
Behnam <b.rajabifard@gmail.com>
2018-09-29 17:48:09 +0330

    Add Nav component

---
+++ components/Nav.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import styled from 'styled-components'
+
+// Locals
+import Logo from './shared/Logo'
+
+const Nav = () => (
+  <header>
+    <div>
+      <Logo />
+    </div>
+    <nav>
+      <a href="#">Nav</a>
+    </nav>
+  </header>
+)
+
+export default Nav
+
+const StyledNav = styled.nav``
