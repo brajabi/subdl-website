c292f6328e58934c52267011567bf27a59503a8c
Behnam <b.rajabifard@gmail.com>
2018-09-30 11:54:15 +0330

    Add Nav items

--- components/Nav.js
+++ components/Nav.js
@@ -5,16 +5,54 @@ import styled from 'styled-components'
 import Logo from './shared/Logo'
 
 const Nav = () => (
-  <header>
-    <div>
+  <HeaderContainer>
+    <LogoContainer>
       <Logo />
-    </div>
-    <nav>
-      <a href="#">Nav</a>
-    </nav>
-  </header>
+    </LogoContainer>
+    <StyledNav>
+      <NavItems href="#">discover</NavItems>
+      <NavItems href="#">popular</NavItems>
+      <NavItems href="#">latest Movies</NavItems>
+    </StyledNav>
+    <RightNav>
+      <NavItems href="#">login</NavItems>
+      <BetweenText>or</BetweenText>
+      <NavItems href="#">signup</NavItems>
+    </RightNav>
+  </HeaderContainer>
 )
 
 export default Nav
 
-const StyledNav = styled.nav``
+const HeaderContainer = styled.header`
+  display: flex;
+`
+
+const LogoContainer = styled.div``
+
+const StyledNav = styled.nav`
+  flex-grow: 1;
+
+  display: flex;
+  align-items: flex-end;
+  padding: 0 30px 10px;
+`
+
+const RightNav = styled.div`
+  display: flex;
+  align-items: flex-end;
+  padding-bottom: 10px;
+`
+
+const NavItems = styled.a`
+  margin-right: 10px;
+  text-decoration: none;
+  color: ${p => p.theme.dark};
+
+  font-family: ${p => p.theme.mainFont};
+  font-size: 14px;
+`
+
+const BetweenText = styled.div`
+  margin: 0 19px;
+`
