7b8c4b9212a322772e1b5cbdc4f6586b71eb8d7f
Behnam <b.rajabifard@gmail.com>
2018-09-30 21:41:44 +0330

    feat: add some font size

--- utils/theme.js
+++ utils/theme.js
@@ -1,3 +1,14 @@
 export default {
-  MainFont: 'Roboto',
+  mainFont: 'Roboto',
+  dark: '#000',
+
+  //fontSizes
+  XXLarge: '40px',
+  XLarge: '24px',
+  Large: '19px',
+  meduim: '17px',
+  normal15: '15px',
+  normal: '14px',
+  small: '13px',
+  smaller: '11px',
 }
