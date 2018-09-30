623c2efef5fa42c227fdb702288b2b4dcd3df3d8
Behnam <b.rajabifard@gmail.com>
2018-09-30 21:39:54 +0330

    Add SVG & main image

---
+++ components/shared/BoltIcon.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const BoltIcon = props => (
+  <svg width={8} height={13} fill="none" {...props}>
+    <path
+      d="M7.21 0L0 7.392h2.674L.619 13 8 5.608H5.326L7.209 0z"
+      fill="#BCBCBC"
+    />
+  </svg>
+)
+
+export default BoltIcon
