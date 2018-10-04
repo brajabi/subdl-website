f592894823203ce595a0f014a7c01f3879ccf262
Behnam <b.rajabifard@gmail.com>
2018-10-04 10:44:23 +0330

    Add Chooselanguage component

---
+++ components/searchBox/ChooseLanguage.js
@@ -0,0 +1,39 @@
+import styled, { css } from 'styled-components'
+
+// Locals
+import BoltIcon from '../shared/BoltIcon'
+
+const ChooseLanguage = () => (
+  <Wrapper>
+    <BoltIcon />
+    <Title>Choose Language</Title>
+  </Wrapper>
+)
+
+export default ChooseLanguage
+
+// Styles
+const Wrapper = styled.div`
+  width: 100%;
+  min-height: 20px;
+
+  display: flex;
+  align-items: center;
+  margin-top: 9px;
+  flex-wrap: wrap;
+
+  cursor: pointer;
+  path {
+    fill: #fff;
+  }
+`
+
+const Title = styled.span`
+  margin-left: 5px;
+  font-family: ${p => p.theme.mainFont};
+  font-size: ${p => p.theme.small};
+  font-style: italic;
+  font-weight: 300;
+
+  color: #fff;
+`
