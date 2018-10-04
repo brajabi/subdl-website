6daac0387c4eb079410534a4ef06e49f1b66d56a
Behnam <b.rajabifard@gmail.com>
2018-10-04 11:49:46 +0330

    Fix container for footer

--- components/shared/Containers.js
+++ components/shared/Containers.js
@@ -7,5 +7,10 @@ export const MainContainer = styled.div`
   height: 100%;
   margin: 0px auto;
   overflow: hidden;
+  padding-bottom: 30px;
+`
+
+export const Wrapper = styled.div`
+  overflow: hidden;
   padding: 0 35px;
 `
