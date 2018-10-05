a3fc49d47db607113148beba6a5bb64869e09667
Behnam <b.rajabifard@gmail.com>
2018-10-05 22:08:49 +0330

    Add another mode

--- components/Header.js
+++ components/Header.js
@@ -2,19 +2,21 @@ import styled, { css } from 'styled-components'
 
 // locals
 import Nav from '../components/Nav'
-import { Wrapper } from '../components/shared/Containers'
+import { Wrapper, ContentWrapper } from '../components/shared/Containers'
 import SearchBox from '../components/searchBox/SearchBox'
 import Space from '../components/shared/Space'
 
-const Header = ({ secondMode }) => (
-  <Container secondMode={secondMode}>
+const Header = ({ mode }) => (
+  <Container mode={mode}>
     <Wrapper>
-      <Nav secondMode />
-      {secondMode && (
+      <Nav mode={mode}>{mode == 'movie' && <SearchBox mode={mode} />}</Nav>
+      {mode == 'searchList' && (
         <>
-          <Space height="20" />
-          <SearchBox fullWidth />
-          <Space height="20" />
+          <ContentWrapper>
+            <Space height="20" />
+            <SearchBox secondMode />
+            <Space height="20" />
+          </ContentWrapper>
         </>
       )}
     </Wrapper>
@@ -26,8 +28,17 @@ export default Header
 // Style
 export const Container = styled.header`
   ${p =>
-    p.secondMode &&
+    p.mode == 'searchList' &&
     css`
-      background: linear-gradient(180deg, #1b82e1 0%, #3ac5d8 100%);
+      background: linear-gradient(180deg, #e0f3ee 0%, #f5f5f5 21.75%),
+        linear-gradient(180deg, #1b82e1 0%, #3ac5d8 100%);
+    `};
+
+  ${p =>
+    p.mode == 'movie' &&
+    css`
+      background: linear-gradient(180deg, #e0f3ee 0%, #f5f5f5 21.75%),
+        linear-gradient(180deg, #1b82e1 0%, #3ac5d8 100%);
+      min-height: 220px;
     `};
 `
