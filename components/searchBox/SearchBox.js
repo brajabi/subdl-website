25ffae2b8dda9f375e2d739014203ce739656417
Behnam <b.rajabifard@gmail.com>
2018-10-09 18:41:06 +0330

    Add topspace in index mode

--- components/searchBox/SearchBox.js
+++ components/searchBox/SearchBox.js
@@ -1,9 +1,12 @@
+// Locals
 import InputBox from './InputBox'
 import Trends from './Trends'
 import ChooseLanguage from './ChooseLanguage'
+import Space from '../shared/Space'
 
 const SearchBox = ({ mode }) => (
   <>
+    {!mode && <Space height="30" />}
     <InputBox mode={mode} />
     {!mode && <Trends />}
     {mode == 'searchList' && <ChooseLanguage />}
