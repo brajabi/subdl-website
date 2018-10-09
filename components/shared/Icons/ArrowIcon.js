a11badf37e28945bd3fc1eaaf6202e07050b41e3
Behnam <b.rajabifard@gmail.com>
2018-10-09 18:42:06 +0330

    Add ArrowIcon component

---
+++ components/shared/Icons/ArrowIcon.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ArrowIcon = props => (
+  <svg viewBox="0 0 41.999 41.999" width="1em" height="1em" {...props}>
+    <path d="M36.068 20.176l-29-20A1 1 0 1 0 5.5.999v40a1 1 0 0 0 1.568.823l29-20a.999.999 0 0 0 0-1.646z" />
+  </svg>
+)
+
+export default ArrowIcon
